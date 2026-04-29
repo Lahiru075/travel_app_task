9d1ded4fd2db0b140d677f8b90064e5b24ce9456
Lahiru075 <sanjeewalahiru057@gmail.com>
2026-04-29 11:14:24 +0530

    update place model for api integration and add dependencies

--- lib/models/place_model.dart
+++ lib/models/place_model.dart
@@ -1,17 +1,39 @@
 class Place {
-  final String name;
+  final String id;
+  final String title;
   final String image;
   final String location;
   final String rating;
   final String description;
 
   Place({
-    required this.name,
+    required this.id,
+    required this.title,
     required this.image,
     required this.location,
     required this.rating,
     required this.description,
   });
-}
 
-List<Place> globalFavorites = [];
+  factory Place.formJson(Map<String, dynamic> json) {
+    return Place(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      image: json['image'] as String,
+      location: json['location'] as String,
+      rating: json['rating'] as String,
+      description: json['description'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'image': image,
+      'location': location,
+      'rating': rating,
+      'description': description,
+    };
+  }
+}
